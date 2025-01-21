a5613d1662b4cda90c7fca0b485838a0a488cafb
veresoora <lahanabira@gmail.com>
2025-01-22 02:18:43 +0700

    membuat PenyediaViewModel

---
+++ app/src/main/java/com/nadia/ucpakhir/ui/PenyediaViewModel.kt
@@ -0,0 +1,19 @@
+package com.nadia.ucpakhir.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.nadia.ucpakhir.PertanianApp
+import com.nadia.ucpakhir.ui.tanaman.viewmodel.HomeTanamanViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeTanamanViewModel(pertanianApp().container.tanamanRepository)
+        }
+    }
+}
+fun CreationExtras.pertanianApp(): PertanianApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PertanianApp)
