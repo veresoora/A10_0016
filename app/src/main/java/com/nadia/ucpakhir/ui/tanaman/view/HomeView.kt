5dd6f2ffd70eb053958694c75163c5ad4cd362ec
veresoora <lahanabira@gmail.com>
2025-01-22 22:34:22 +0700

    membuat HomeView untuk Tanaman

---
+++ app/src/main/java/com/nadia/ucpakhir/ui/tanaman/view/HomeView.kt
@@ -0,0 +1,325 @@
+package com.nadia.ucpakhir.ui.tanaman.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.nadia.ucpakhir.R
+import com.nadia.ucpakhir.model.Tanaman
+import com.nadia.ucpakhir.ui.PenyediaViewModel
+import com.nadia.ucpakhir.ui.customwidget.CostumeTopAppBar
+import com.nadia.ucpakhir.ui.navigation.DestinasiNavigasi
+import com.nadia.ucpakhir.ui.tanaman.viewmodel.HomeTanamanUiState
+import com.nadia.ucpakhir.ui.tanaman.viewmodel.HomeTanamanViewModel
+
+object DestinasiHomeTanaman : DestinasiNavigasi {
+    override val route = "home_tanaman"
+    override val titleRes = "Daftar Tanaman"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTanamanScreen(
+    navigateToltemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeTanamanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHomeTanaman.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getTanaman()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton (
+                onClick = navigateToltemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ){
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Tanaman")
+            }
+        },
+    ){ innerPadding ->
+        HomeTanamanStatus (
+            homeTanamanUiState = viewModel.tanamanUiState,
+            retryAction = { viewModel.getTanaman() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteTanaman(it.idTanaman)
+                viewModel.getTanaman()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeTanamanStatus(
+    homeTanamanUiState: HomeTanamanUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Tanaman) -> Unit = {},
+    onDetailClick: (Int) -> Unit
+){
+    when (homeTanamanUiState){
+        is HomeTanamanUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeTanamanUiState.Success ->
+            if (homeTanamanUiState.tanaman.isEmpty()) {
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Kontak")
+                }
+            }else {
+                TanamanLayout(
+                    tanaman = homeTanamanUiState.tanaman,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.idTanaman)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeTanamanUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.iconsloading),
+        contentDescription = "loading"
+    )
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.iconserror),
+            contentDescription = ""
+        )
+        Text(
+            text = "Loading Failed",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button (
+            onClick = retryAction
+        ){
+            Text("Retry")
+        }
+    }
+}
+
+
+@Composable
+fun TanamanLayout(
+    tanaman: List<Tanaman>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Tanaman) -> Unit,
+    onDeleteClick: (Tanaman) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(tanaman) { tnmn ->
+            TanamanCard(
+                tanaman = tnmn,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(tnmn) },
+                onDeleteClick = {
+                    onDeleteClick(tnmn)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun TanamanCard(
+    tanaman: Tanaman,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Tanaman) -> Unit = {},
+    onEditClick: (Tanaman) -> Unit = {}
+) {
+    var deleteConfirmation by rememberSaveable { mutableStateOf(false) }
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .shadow(elevation = 8.dp, shape = RoundedCornerShape(16.dp)),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(
+                    Color(0xFF00796B)
+                )
+                .padding(16.dp)
+                .fillMaxWidth()
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.plant),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = tanaman.namaTanaman,
+                        style = MaterialTheme.typography.titleLarge.copy(color = Color.White),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Spacer(Modifier.weight(1f))
+                    IconButton(onClick = { deleteConfirmation = true }) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete",
+                            tint = Color.Red
+                        )
+                    }
+                    IconButton(onClick = { onEditClick(tanaman) }) {
+                        Icon(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = "Edit",
+                            tint = Color.White
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider(
+                    color = Color.White.copy(alpha = 0.7f),
+                    thickness = 1.dp
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(R.drawable.weekend),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = tanaman.periodeTanam,
+                        style = MaterialTheme.typography.bodyLarge.copy(color = Color.White),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+        }
+    }
+
+    if (deleteConfirmation) {
+        KonfirmasiDeleteData(
+            onDeleteConfirm = {
+                deleteConfirmation = false
+                onDeleteClick(tanaman)
+            },
+            onDeleteCancel = {
+                deleteConfirmation = false
+            },
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+
+@Composable
+private fun KonfirmasiDeleteData (
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = {},
+        title = { Text("Hapus Data") },
+        text = { Text("Apakah Anda benar-benar ingin menghapus data ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton (onClick = onDeleteCancel) {
+                Text(text = "Batal")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Ya")
+            }
+        }
+    )
+}
