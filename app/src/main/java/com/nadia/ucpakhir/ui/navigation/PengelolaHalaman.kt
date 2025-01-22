16f079319cb8261a34e70c2c6b3979acd91a0ec1
veresoora <lahanabira@gmail.com>
2025-01-22 22:35:56 +0700

    membuat PengelolaHalaman untuk navigasi

---
+++ app/src/main/java/com/nadia/ucpakhir/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,35 @@
+package com.nadia.ucpakhir.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.nadia.ucpakhir.ui.tanaman.view.DestinasiHomeTanaman
+import com.nadia.ucpakhir.ui.tanaman.view.HomeTanamanScreen
+
+@Composable
+fun PengelolaHalamanPertanian(
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHomeTanaman.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHomeTanaman.route){
+            HomeTanamanScreen(
+                navigateToltemEntry = {
+                    //navController.navigate(DestinasiEntry.route)
+                },
+                onDetailClick = { //nim ->
+//                    navController.navigate("${DestinasiDetail.route}/$nim")
+//                    println("PengelolaHalaman: nim = $nim")
+                }
+            )
+        }
+    }
+}
