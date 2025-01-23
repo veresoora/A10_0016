6d440db0b4ca530bbe9a3c18fcd045175a755625
veresoora <lahanabira@gmail.com>
2025-01-23 16:58:42 +0700

    membuat interface PekerjaRepository dan class NetworkPekerjaRepository

---
+++ app/src/main/java/com/nadia/ucpakhir/repository/PekerjaRepository.kt
@@ -0,0 +1,54 @@
+package com.nadia.ucpakhir.repository
+
+import com.nadia.ucpakhir.model.AllPekerjaResponse
+import com.nadia.ucpakhir.model.Pekerja
+import com.nadia.ucpakhir.service_api.PekerjaService
+import okio.IOException
+
+interface PekerjaRepository {
+
+    suspend fun insertPekerja(pekerja: Pekerja)
+
+    suspend fun getPekerja(): AllPekerjaResponse
+
+    suspend fun updatePekerja(idPekerja: Int, pekerja: Pekerja)
+
+    suspend fun deletePekerja(idPekerja: Int)
+
+    suspend fun getPekerjaID(idPekerja: Int): Pekerja
+}
+
+class NetworkPekerjaRepository (
+    private val pekerjaService: PekerjaService
+) : PekerjaRepository{
+
+    override suspend fun insertPekerja(pekerja: Pekerja) {
+        pekerjaService.insertPekerja(pekerja)
+    }
+
+    override suspend fun updatePekerja(idPekerja: Int, pekerja: Pekerja) {
+        pekerjaService.updatePekerja(idPekerja, pekerja)
+    }
+
+    override suspend fun deletePekerja(idPekerja: Int) {
+        try {
+            val response = pekerjaService.deletePekerja(idPekerja)
+            if (response.isSuccessful) {
+                throw IOException("Gagal untuk menghapus pekerja. HTTP Status code: " +
+                        "${response.code()}")
+            }else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception) {
+            throw  e
+        }
+    }
+
+    override suspend fun getPekerja(): AllPekerjaResponse =
+        pekerjaService.getAllPekerja()
+
+    override suspend fun getPekerjaID(idPekerja: Int): Pekerja {
+        return pekerjaService.getPekerjaID(idPekerja).data
+    }
+}
