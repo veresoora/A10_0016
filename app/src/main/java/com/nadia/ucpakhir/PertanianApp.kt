02dcf0bc033c1aa0ff99bfad00d3e02ca8e43aad
veresoora <lahanabira@gmail.com>
2025-01-23 17:37:09 +0700

    menambahkan inisialisasi containerPekerja dengan instance PekerjaContainer

--- app/src/main/java/com/nadia/ucpakhir/PertanianApp.kt
+++ app/src/main/java/com/nadia/ucpakhir/PertanianApp.kt
@@ -1,13 +1,17 @@
 package com.nadia.ucpakhir
 
 import android.app.Application
+import com.nadia.ucpakhir.di.PekerjaAppContainer
+import com.nadia.ucpakhir.di.PekerjaContainer
 import com.nadia.ucpakhir.di.TanamanAppContainer
 import com.nadia.ucpakhir.di.TanamanContainer
 
 class PertanianApp: Application() {
     lateinit var container: TanamanAppContainer
+    lateinit var containerPekerja: PekerjaAppContainer
     override fun onCreate() {
         super.onCreate()
         container=TanamanContainer()
+        containerPekerja=PekerjaContainer()
     }
 }
