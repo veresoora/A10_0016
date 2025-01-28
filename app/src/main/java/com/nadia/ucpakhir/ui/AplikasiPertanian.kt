dbbd2d2c743db60da9c1530ae3d4733c7bcc1176
veresoora <lahanabira@gmail.com>
2025-01-28 07:42:13 +0700

    menghapus variabel scrollBehavior dan modifier scaffold

--- app/src/main/java/com/nadia/ucpakhir/ui/AplikasiPertanian.kt
+++ app/src/main/java/com/nadia/ucpakhir/ui/AplikasiPertanian.kt
@@ -1,5 +1,6 @@
 package com.nadia.ucpakhir.ui
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,15 +12,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.nadia.ucpakhir.ui.navigation.PengelolaHalamanPertanian
 
+@SuppressLint("NewApi")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AplikasiPertanian(
     modifier: Modifier = Modifier
 ){
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-    ){
+    Scaffold(){
         Surface(
             modifier = Modifier
                 .fillMaxSize()
