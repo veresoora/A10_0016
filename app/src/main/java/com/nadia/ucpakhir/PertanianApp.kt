696d1ed917ec7ba0b4223bf96ca83ebdea76fea3
veresoora <lahanabira@gmail.com>
2025-01-28 07:07:21 +0700

    menambahkan inisialisasi containerAktivitasPertanian dan containeCatatanPanen dengan instance AktivitasPertanianContainer dan CatatanPanenContainer

--- app/src/main/java/com/nadia/ucpakhir/PertanianApp.kt
+++ app/src/main/java/com/nadia/ucpakhir/PertanianApp.kt
@@ -1,6 +1,10 @@
 package com.nadia.ucpakhir
 
 import android.app.Application
+import com.nadia.ucpakhir.di.AktivitasPertanianAppContainer
+import com.nadia.ucpakhir.di.AktivitasPertanianContainer
+import com.nadia.ucpakhir.di.CatatanPanenAppContainer
+import com.nadia.ucpakhir.di.CatatanPanenContainer
 import com.nadia.ucpakhir.di.PekerjaAppContainer
 import com.nadia.ucpakhir.di.PekerjaContainer
 import com.nadia.ucpakhir.di.TanamanAppContainer
@@ -9,9 +13,13 @@ import com.nadia.ucpakhir.di.TanamanContainer
 class PertanianApp: Application() {
     lateinit var container: TanamanAppContainer
     lateinit var containerPekerja: PekerjaAppContainer
+    lateinit var containerAktivitasPertanian: AktivitasPertanianAppContainer
+    lateinit var containerCatatanPanen: CatatanPanenAppContainer
     override fun onCreate() {
         super.onCreate()
         container=TanamanContainer()
         containerPekerja=PekerjaContainer()
+        containerAktivitasPertanian=AktivitasPertanianContainer()
+        containerCatatanPanen=CatatanPanenContainer()
     }
 }
